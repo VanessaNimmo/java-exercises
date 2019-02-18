c110d0ae65b6c55feda2b6b3572d52944b922469
Vanessa Nimmo <vanessanimmo@live.com>
2019-02-18 11:28:13 +1100

    Added a pay class

--- payslip1/src/com/company/Main.java
+++ payslip1/src/com/company/Main.java
@@ -19,16 +19,11 @@ class App {
         System.out.println("Welcome to the payslip generator!");
 
         // Get info from person, convert from strings to usable stuff
-        InputStream source = System.in;
-        Scanner reader = new Scanner(source);
 
         Employee person = new Employee();
 
-        System.out.println("Please enter your first name:");
-        String firstName = reader.nextLine();
-
-        System.out.println("Please enter your last name:");
-        String lastName = reader.nextLine();
+        String firstName = getString("first name");
+        String lastName = getString("last name");
 
         person.setFirstName(firstName);
         person.setLastName(lastName);
@@ -38,14 +33,35 @@ class App {
         int annualSalary = getPositiveInteger("annual salary");
         float superRate = getPositiveFloat("super rate");
 
+        String paymentStartDate = getString("payment start date");
+        String paymentEndDate = getString("payment end date");
 
         System.out.println(annualSalary);
         System.out.println(superRate);
+        System.out.println(paymentStartDate);
+        System.out.println(paymentEndDate);
 
         // Do calculations
 
+        Pay payDetails = new Pay();
+        payDetails.setSalary(annualSalary);
+        payDetails.setSuper(superRate);
+        payDetails.setPayPeriodStart(paymentStartDate);
+        payDetails.setPayPeriodEnd(paymentEndDate);
+
         // Present information
 
+        
+
+    }
+
+    private String getString(String stringFor) {
+
+        InputStream source = System.in;
+        Scanner reader = new Scanner(source);
+
+        System.out.println("Please enter your " + stringFor + ":");
+        return reader.nextLine();
     }
 
     private int getPositiveInteger(String numberFor) {
@@ -84,6 +100,7 @@ class App {
 
 
 
+
 }
 
 class Employee {
@@ -101,5 +118,39 @@ class Employee {
     }
 
     public String getLastName() { return this.lastName; }
+
+}
+
+class Pay {
+
+    public Int annualSalary, grossMonthlySalary, incomeTax, netIncome, superRate, superAmount;
+    public String payPeriodStart, payPeriodEnd;
+
+    public void setSalary(Int annualSalary) {
+        this.annualSalary = annualSalary;
+        this.grossMonthlySalary = Math.round(annualSalary/12.0);
+        this.incomeTax = this.calculateTax();
+        this.netIncome = grossMonthlySalary - incomeTax;
+    };
+
+    public void setSuper(float superRate) {
+        this.superRate = superRate;
+        this.superAmount = Math.round(this.grossMonthlySalary * superRate);
+    }
+
+    public void setPayPeriodStart(String payPeriodStart) { this.payPeriodStart = payPeriodStart; }
+    public void setPayPeriodEnd(String payPeriodEnd) { this.payPeriodEnd = payPeriodEnd; }
+
+    public String getPayPeriod() { return (this.payPeriodStart + " - " + this.payPeriodEnd); }
+    public Int getGrossMonthlySalary() { return this.grossMonthlySalary; }
+    public Int getIncomeTax() { return this.incomeTax; }
+    public Int getNetIncome() { return this.netIncome; }
+    public Int getSuperAmount() { return this.superAmount; }
+
+
+    private int calculateTax() {
+        // Do tax calculations here
+        return 5;
+    }
 
 }
