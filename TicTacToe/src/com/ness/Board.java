09b27b78e52ff03bd12d1b2132992d8efc57c984
Vanessa Nimmo <vanessanimmo@live.com>
2019-02-25 15:08:30 +1100

    Board now knows if it is full

--- TicTacToe/src/com/ness/Board.java
+++ TicTacToe/src/com/ness/Board.java
@@ -24,6 +24,13 @@ public class Board {
         if(moveIsOnBoard(column, row) && this.state[column][row].isEmpty) {
             this.state[column][row].makeEntry(entry);
         }
+        this.checkIfFull();
     }
 
+    private void checkIfFull() {
+        this.isFull = true;
+        for(int i=0; i<this.size; i++) {
+            for(int j=0; j<this.size; j++) if(this.state[i][j].isEmpty) this.isFull = false;
+        }
+    }
 }
