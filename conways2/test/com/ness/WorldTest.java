9ebf4c105afbd1ac9d056bb2604895b6fc5e795d
Vanessa Nimmo <vanessanimmo@live.com>
2019-03-08 11:27:00 +1100

    Testing around the world-board contract and stubbing out the board

--- conways2/test/com/ness/WorldTest.java
+++ conways2/test/com/ness/WorldTest.java
@@ -1,18 +1,45 @@
 package com.ness;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class WorldTest {
 
+    private InputOutput io;
+
+    @Before
+    public void setUp() {
+        this.io = new IO();
+    }
+
     @Test
     public void shouldAcceptInitialStateFromAUser() {
-        InputOutput io = new IO();
         World newWorld = new World(io);
 
         boolean[][] result = newWorld.io.getInitialState();
 
+        boolean[][] expected = {{false, true, false, false}, {true, true, true, false}, {false, false, false, true}, {true, false, true, false}};
+
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void shouldMakeNewWorldRepresentationFromInitialState() {
+        World newWorld = new World(io);
+
+        Board currentState = new Board(newWorld.io.getInitialState());
+
+        assertNotNull(currentState);
+    }
+
+    @Test
+    public void shouldBeAbleToRetrieveStateFromCurrentBoard() {
+        World newWorld = new World(io);
+
+        Board currentState = new Board(newWorld.io.getInitialState());
+        boolean[][] result = currentState.getLiveCells();
         boolean[][] expected = {{false, true, false, false}, {true, true, true, false}, {false, false, false, true}, {true, false, true, false}};
 
         assertArrayEquals(expected, result);
@@ -24,6 +51,17 @@ public class WorldTest {
             boolean[][] initialLiveCells = {{false, true, false, false}, {true, true, true, false}, {false, false, false, true}, {true, false, true, false}};
             return initialLiveCells;
         }
+    }
+
+    class Board {
+
+        private final boolean[][] liveCells;
+
+        Board(boolean[][] initialState) {
+            this.liveCells = initialState;
+        }
+
+        boolean[][] getLiveCells() { return this.liveCells;}
     }
 
 }
