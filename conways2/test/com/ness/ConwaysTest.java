8eac94e3b9a348f2478cfa2587651ce8e4116ec0
Vanessa Nimmo <vanessanimmo@live.com>
2019-03-19 13:01:50 +1100

    Began setting up tests for an intermediate class between the app and the functional core that runs the number of ticks

--- conways2/test/com/ness/ConwaysTest.java
+++ conways2/test/com/ness/ConwaysTest.java
@@ -1,7 +1,20 @@
 package com.ness;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ConwaysTest {
+
+    @Test
+    public void shouldBeInitialisedWithAStartingGridAndNumOfTicksAndSleepTime() {
+        boolean[][] startingCells = {{false}};
+        Grid initialState = new Grid(startingCells);
+        int numberOfTicks = 5;
+        int sleepTimeInMs = 500;
+        Conways game = new Conways(initialState, numberOfTicks, sleepTimeInMs);
+
+        assertNotNull(game);
+    }
 
 }
