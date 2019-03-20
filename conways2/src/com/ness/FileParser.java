d71265e567f66239d0897dd213ff0dc80d2ee676
Vanessa Nimmo <vanessanimmo@live.com>
2019-03-20 13:51:44 +1100

    Converted the try-catch block in the FileParser class into a try-with-resources block

--- conways2/src/com/ness/FileParser.java
+++ conways2/src/com/ness/FileParser.java
@@ -3,6 +3,7 @@ package com.ness;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 class FileParser {
@@ -18,21 +19,33 @@ class FileParser {
     Grid getInitialState() {
         String line = null;
         ArrayList<int[]> numberPairs = new ArrayList<>();
-        try {
-            FileReader fileReader = new FileReader(initialGridInfo);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            while((line = bufferedReader.readLine()) != null) {
-                if(validator.validInputString(line)) {
+        try (FileReader fileReader = new FileReader(initialGridInfo);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            while ((line = bufferedReader.readLine()) != null) {
+                if (validator.validInputString(line)) {
                     numberPairs.add(parseIntegerPair(line));
                 }
             }
-        } catch (java.io.IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         int[] gridSize = numberPairs.get(0);
-        return new Grid(new boolean[gridSize[0]][gridSize[1]]);
+        numberPairs.remove(0);
+        boolean[][] initialState = new boolean[gridSize[0]][gridSize[1]];
+        if(numberPairs.size() >= 1) {
+            initialState = addLiveCells(numberPairs, initialState);
+        }
+        return new Grid(initialState);
     }
 
+    private boolean[][] addLiveCells(ArrayList<int[]> numberPairs, boolean[][] initialState) {
+        numberPairs.forEach((pair) -> {
+            if (validator.validCellLocation(pair, initialState)) {
+                initialState[pair[1]-1][pair[0]-1] = true;
+            }
+        });
+        return initialState;
+    }
 
     private static int[] parseIntegerPair(String expression) {
         String[] splitInput = expression.split(" ");
--- conways2/test/com/ness/FileParserTest.java
+++ conways2/test/com/ness/FileParserTest.java
@@ -10,13 +10,27 @@ public class FileParserTest {
 
     @Test
     public void aOneLineFileShouldProduceAOneByOneGridWithOneDeadCell() {
-        File initialGridInfo = new File("setup1.txt");
+        File initialGridInfo = new File("test/com/ness/setup1.txt");
         Validator validator = new Validator(56);
         FileParser reader = new FileParser(initialGridInfo, validator);
 
         Grid result = reader.getInitialState();
 
         Grid expected = new Grid(new boolean[1][1]);
+
+        assertEquals(String.valueOf(expected), String.valueOf(result));
+    }
+
+    @Test
+    public void aSecondLineInTheFileShouldBeAddedToTheGridAsALiveCellIfValid() {
+        File initialGridInfo = new File("test/com/ness/setup1livecell.txt");
+        Validator validator = new Validator(56);
+        FileParser parser = new FileParser(initialGridInfo, validator);
+
+        Grid result = parser.getInitialState();
+        boolean[][] liveCells = new boolean[1][1];
+        liveCells[0][0] = true;
+        Grid expected = new Grid(liveCells);
 
         assertEquals(String.valueOf(expected), String.valueOf(result));
     }
